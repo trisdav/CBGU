bd138064d9feaeeb8ac49cf028c2af5ed34f12d9
HeyubCS <tridav95@gmail.com>
2017-11-17 18:51:16 -0600

    Cafeteria nearly done.

--- Cafeteria.lua
+++ Cafeteria.lua
@@ -1,3 +1,4 @@
+-- Written by Tristan Davis
 local composer = require( "composer" )
 local physics = require("physics")
 local backButtons = require("objects.menu_button")
@@ -19,9 +20,10 @@ local cracker = "arts/cracker.png"
 local sandwich = "arts/sandwich.png"
 local candy = "arts/candy.png"
 local spider = "arts/spider.png"
-local spoon = "arts/spoon/png"
+local spoon = "arts/spoon.png"
 local fly = "arts/fly.png"
 local grapes = "arts/grapes.png"
+local bean = "arts/bean.png"
 
 local function positiveObject(event)
 	if(event.phase == "moved") then
@@ -43,44 +45,78 @@ local function createObject()
 	local n =  math.random(3,5)
 	for i=1,n,1 do
 		local theObject;
-		local o = math.random(1, 6); -- Object
-		theObject = display.newRect(math.random(200, display.contentWidth-200), display.contentHeight + 100, 100,100)
-		theObject:setFillColor(math.random(), math.random(), math.random())
+		local o = math.random(1, 9); -- Object
 
-		physics.addBody(theObject, "dynamic", {isSensor = true})
-		theObject:setLinearVelocity(math.random(-100,100), -150 * math.random(4,6));
+		if(o == 1) then -- apple
+			theObject = display.newImage(apple);
+			theObject.x = math.random(200, display.contentWidth-200);
+			theObject.y = display.contentHeight + 100;
+			theObject:addEventListener("touch", positiveObject)
+			physics.addBody(theObject, "dynamic", {isSensor = true})
+		elseif(o == 2) then -- cracker
+			theObject = display.newImage(cracker);
+			theObject.x = math.random(200, display.contentWidth-200);
+			theObject.y = display.contentHeight + 100;
+			theObject:addEventListener("touch", positiveObject)
+			physics.addBody(theObject, "dynamic", {isSensor = true})
+		elseif(o == 3) then -- sandwich
+			theObject = display.newImage(sandwich);
+			theObject.x = math.random(200, display.contentWidth-200);
+			theObject.y = display.contentHeight + 100;
+			theObject:addEventListener("touch", positiveObject)
+			physics.addBody(theObject, "dynamic", {isSensor = true})
+		elseif(o == 4) then -- candy
+			theObject = display.newImage(candy);
+			theObject.x = math.random(200, display.contentWidth-200);
+			theObject.y = display.contentHeight + 100;
+			theObject:addEventListener("touch", positiveObject)
+			physics.addBody(theObject, "dynamic", {isSensor = true})
+		elseif(o == 5) then -- spider
+			theObject = display.newImage(spider);
+			theObject.x = math.random(200, display.contentWidth-200);
+			theObject.y = display.contentHeight + 100;
+			theObject:addEventListener("touch", negativeObject)
+			physics.addBody(theObject, "dynamic", {isSensor = true})
+		elseif(o == 6) then -- spoon
+			theObject = display.newImage(spoon);
+			theObject.x = math.random(200, display.contentWidth-200);
+			theObject.y = display.contentHeight + 100;
+			theObject:addEventListener("touch", negativeObject)
+			physics.addBody(theObject, "dynamic", {isSensor = true})
+		elseif(o == 7) then -- fly
+			theObject = display.newImage(fly);
+			theObject.x = math.random(200, display.contentWidth-200);
+			theObject.y = display.contentHeight + 100;
+			theObject:addEventListener("touch", negativeObject)
+			physics.addBody(theObject, "dynamic", {isSensor = true})
+		elseif(o == 8) then -- grapes
+			theObject = display.newImage(grapes);
+			theObject.x = math.random(200, display.contentWidth-200);
+			theObject.y = display.contentHeight + 100;
+			theObject:addEventListener("touch", positiveObject)
+			physics.addBody(theObject, "dynamic", {isSensor = true})
+		elseif(o == 9) then -- bean
+			theObject = display.newImage(bean);
+			theObject.x = math.random(200, display.contentWidth-200);
+			theObject.y = display.contentHeight + 100;
+			theObject:addEventListener("touch", positiveObject)
+			physics.addBody(theObject, "dynamic", {isSensor = true})
 
-		if(o == 1) then
-			theObject:setFillColor(1,1,.5)
-			theObject:addEventListener("touch", negativeObject)
-		elseif(o == 2) then
-			theObject:setFillColor(0,0,1)
-			theObject:addEventListener("touch", positiveObject)
-		elseif(o == 3) then
-			theObject:setFillColor(1,0,0)
-			theObject:addEventListener("touch", negativeObject)
-		elseif(o == 4) then
-			theObject:setFillColor(0,1,0)
-			theObject:addEventListener("touch", positiveObject)
-		elseif(o == 5) then
-			theObject:setFillColor(1,.5,.5)
-			theObject:addEventListener("touch", negativeObject)
-		elseif(o == 6) then
-			theObject:setFillColor(1,0,1)
-			theObject:addEventListener("touch", positiveObject)
-		--elseif(o == 7) then
-		--elseif(o == 8) then
-		--elseif(o == 9) then
 		--elseif(o == 10) then
 		else
+			print("Random food out of bounds.")
 		end
+		theObject:setLinearVelocity(math.random(-100,100), -150 * math.random(4,6));
+		theObject.angularVelocity = math.random(5,15)
 	end
-	--return theObject;
+		--return theObject;
 	local function kill()
 		display.remove(theObject);
 	end
 	timer.performWithDelay(1000 * 10 , kill);
 end
+
+
 local objectGenerator;
 local function gameOver(event)
 	-- Game over things.
@@ -106,21 +142,6 @@ function scene:create( event )
 
     local sceneGroup = self.view
     physics.start();
-    --physics.setDrawMode("hybrid")
-    local bg = display.newImage("arts/cafeteriaBG1.png", 360, 640)
-    sceneGroup:insert(bg);
-    local timeBar = display.newImage("arts/barfill.png", 355, 110)
-    timeBar:scale(1.16,1.3)
-    sceneGroup:insert(timeBar);
-   	timeBar:toBack();
-   	bg:toBack();
-   	startGame(timeBar);
-   	back_button = backButtons.newCampus();
-	sceneGroup:insert(back_button.campus);
-	back_button.campus.isVisible = false;
-
-	pointText = display.newText("Score: 0", display.contentCenterX, 40, native.systemFont, 45)
-	pointText:setFillColor(0,0,0)
     -- Code here runs when the scene is first created but has not yet appeared on screen
 
 
@@ -136,7 +157,21 @@ function scene:show( event )
 
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
-
+	    --physics.setDrawMode("hybrid")
+	    local bg = display.newImage("arts/cafeteriaBG1.png", 360, 640)
+	    sceneGroup:insert(bg);
+	    local timeBar = display.newImage("arts/barfill.png", 355, 110)
+	    timeBar:scale(1.16,1.3)
+	    sceneGroup:insert(timeBar);
+	   	timeBar:toBack();
+	   	bg:toBack();
+	   	startGame(timeBar);
+	   	back_button = backButtons.newCampus();
+		sceneGroup:insert(back_button.campus);
+		back_button.campus.isVisible = false;
+		pointText = display.newText("Score: 0", display.contentCenterX, 40, native.systemFont, 45)
+		pointText:setFillColor(0,0,0)
+		sceneGroup:insert(pointText);
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
 
